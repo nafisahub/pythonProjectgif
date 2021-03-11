3780f40fe6d63b39dd99c35d350c00bef502a200
nafisahub <77121106+nafisahub@users.noreply.github.com>
2021-03-11 21:37:48 +0000

    Initial commit

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+
+color = input ("enter a color")
+plural_noun = input ("Enter a Plural Noun")
+celebrity = input ("enter a celebrity")
+favourite = 
+
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love" + celebrity)
+
+
+
+
+
+
